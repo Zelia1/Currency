3b1e1e4ff25dc089bd6202f3845c328f382db67c
Ваше Имя <zelia>
2021-06-12 11:31:44 +0300

    create bank_update, bank_delete

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,9 @@
 from currency.models import Banks, ContactUs
 
+from currency.forms import BankForm
+
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
 
 
 def hello_world(request):
@@ -36,14 +38,14 @@ def contactus_list(request):
 
 
 def bank_create(request):
-    from currency.forms import BankForm
     
-    form_data = request.GET
-    if form_data:
+    if request.method == 'POST':
+        form_data = request.POST
         form = BankForm(form_data)
         if form.is_valid():
             form.save()
-    else:
+            return HttpResponseRedirect('/currency/banks/')
+    elif request.method == 'GET':
         form = BankForm()
     
     context = {
@@ -51,3 +53,31 @@ def bank_create(request):
         'form': form,
     }
     return render(request, 'bank_create.html', context=context)
+
+
+def bank_update(request, pk):
+    instance = get_object_or_404(Banks, pk=pk)
+    
+    if request.method == 'POST':
+        form_data = request.POST
+        form = BankForm(form_data, instance=instance)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/currency/banks/')
+    elif request.method == 'GET':
+        form = BankForm(instance=instance)
+
+    context = {
+        'message': "Bank update",
+        'form': form,
+    }
+    return render(request, 'bank_update.html', context=context)
+
+
+def bank_delete(request, pk):
+    instance = get_object_or_404(Banks, pk=pk)
+    instance.delete()
+    return HttpResponseRedirect('/currency/banks/')
+    
+
+    return render(request, 'bank_delete.html', context=context)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -2,7 +2,9 @@ from currency.views import (bank_details,
                             banks,
                             contactus_list,
                             hello_world,
-                            bank_create)
+                            bank_create,
+                            bank_update,
+                            bank_delete)
 
 from django.contrib import admin
 from django.urls import path
@@ -19,5 +21,9 @@ urlpatterns = [
 
     path('currency/contactus/', contactus_list),
     
-    path('currency/bank/create/', bank_create)
+    path('currency/bank/create/', bank_create),
+    
+    path('currency/bank/update/<int:pk>/', bank_update),
+    
+    path('currency/bank/delete/<int:pk>/', bank_delete)
 ]
