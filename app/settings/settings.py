a7ea59924cde12d8fb54ffb82cb0bd6295aa9f7f
Ваше Имя <zelia>
2021-06-29 19:43:46 +0300

    add new settings_local.dev.py

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -131,8 +131,8 @@ INTERNAL_IPS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'PavelTest1990@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwerty_123456'
+
+try:
+    from settings.settings_local import *
+except ImportError:
+    print('settings were not found!')
